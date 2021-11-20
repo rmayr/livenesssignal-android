65eb8bdff83e821e55e2a5b1c6cfbd9dac4f2f86
René Mayrhofer <rene@mayrhofer.eu.org>
2021-11-20 14:29:36 +0100

    Remove dead code from LoginActivity - we don't ever ping a server or otherwise validate the passwords, so that model infrastructure is really not going to be needed

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 This is a prototype Android app working with the liveness signal server
 
 TODO:
-* Remove dead code from LoginActivity
 * Allow storing appPassword with keystore integration, including biometric unlock
 * Support connecting through Tor, with a default INS Tor service pre-configured
 * Move settings call from button to burger menu
--- app/src/main/java/at/jku/ins/liveness/android/ui/login/LoginActivity.kt
+++ app/src/main/java/at/jku/ins/liveness/android/ui/login/LoginActivity.kt
@@ -1,29 +1,19 @@
 package at.jku.ins.liveness.android.ui.login
 
-import android.app.Activity
 import android.content.Intent
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.Toast
 import androidx.preference.PreferenceManager
 import at.jku.ins.liveness.android.data.Constants
-import at.jku.ins.liveness.android.data.ProtocolRunData
 import at.jku.ins.liveness.android.ui.main.MainActivity
 import at.jku.ins.liveness.android.databinding.ActivityLoginBinding
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.Security
 
-
 class LoginActivity : AppCompatActivity() {
-
-    //private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
 
     companion object {
@@ -45,93 +35,43 @@ class LoginActivity : AppCompatActivity() {
         val signalPassword = binding.signalPassword
         val start = binding.login
         start.isEnabled = false
-        //val loading = binding.loading
-
-        /*loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)*/
 
         // initialize with defaults from preferences
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
         sharedPreferences.getString(Constants.serverPreference, "").also { server.setText(it.toString()) }
-
-        /*loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
-
-            // disable login buttonVerify unless both server / appPassword is valid
-            start.isEnabled = loginState.isDataValid
-
-            /*if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
-            }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
-            }*/
-        })*/
-
-        /*loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-            val loginResult = it ?: return@Observer
-
-            //loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
-            }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
-            }
-            setResult(Activity.RESULT_OK)
-
-            //Complete and destroy login activity once successful
-            finish()
-        })*/
-
         server.afterTextChanged {
-            /*loginViewModel.loginDataChanged(
-                server.text.toString(),
-                appPassword.text.toString()
-            )*/
+            //Log.d(Constants.LOG_TAG, "Server URL changed to ${server.text}")
         }
 
+        // TODO: load appPassword if it has been stored locally or unlock with biometrics
+
+        // signal password needs to be entered on every app start and is never cached
         signalPassword.apply {
             afterTextChanged {
-                /*loginViewModel.loginDataChanged(
-                    server.text.toString(),
-                    appPassword.text.toString()
-                )*/
+                //Log.d(Constants.LOG_TAG, "Signal password changed to ${signalPassword.text}")
                 start.isEnabled = true
             }
 
             /*setOnEditorActionListener { _, actionId, _ ->
                 when (actionId) {
                     EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
-                            server.text.toString(),
-                            appPassword.text.toString()
-                        )
                 }
                 false
             }*/
 
             start.setOnClickListener {
-                //loading.visibility = View.VISIBLE
-                //loginViewModel.login(server.text.toString(), appPassword.text.toString())
-
                 startMain()
             }
         }
     }
 
     private fun startMain(/*model: ProtocolRunData*/) {
-        //val displayName = model.displayName
-
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra(Constants.intentParamAppPassword, binding.appPassword.text.toString())
         intent.putExtra(Constants.intentParamSignalPassword, binding.signalPassword.text.toString())
         startActivity(intent)
     }
-
-    /*private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
-    }*/
 }
 
 /**
